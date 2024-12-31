a3e2061e223fbfa0dfbd646f73983341a7c975e9
Gregory Triananda <greg3ananda@gmail.com>
2024-12-31 10:37:38 +0700

    added build in vite.config.js, revert changes on vercel.json

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    build: {
+        outDir: 'dist' // Ensure this matches the output directory expected by Vercel
+    },
     plugins: [
         laravel({
             input: [
